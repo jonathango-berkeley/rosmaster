c43d02bff12eb4abec49c68dffabe3ca4f08e4dd
Fernando <fernando_pluma@berkeley.edu>
2025-02-27 15:51:55 +0800

    Aruco Marker detection with pose estimation

---
+++ src/sensor_interface/sensor_interface/mipi_camera_publisher.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+from SunriseRobotLib import Mipi_Camera
+
+class MipiCameraPublisher(Node):
+    def __init__(self):
+        super().__init__('mipi_camera_publisher')
+        self.publisher_ = self.create_publisher(Image, 'camera/image_raw', 10)
+        
+        # Initialize the MIPI Camera
+        self.camera = Mipi_Camera(width=1280, height=720, debug=False)
+        if self.camera.isOpened():
+            self.get_logger().info("Mipi Camera initialized successfully.")
+        else:
+            self.get_logger().error("Failed to initialize Mipi Camera.")
+            raise RuntimeError("Camera initialization failed.")
+
+        self.bridge = CvBridge()
+        
+        # Publish at ~30 FPS
+        self.timer = self.create_timer( 1/30.0 , self.timer_callback)
+
+    def timer_callback(self):
+        ret, frame = self.camera.get_frame()
+        if frame is not None:
+            try:
+                # Convert to ROS Image and publish
+                msg = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
+                self.publisher_.publish(msg)
+                self.get_logger().info("Published a camera image.")
+            except Exception as e:
+                self.get_logger().error(f"Error publishing frame: {e}")
+        else:
+            self.get_logger().error("Failed to capture image.")
+
+    def destroy_node(self):
+        # Release the camera resource when the node is destroyed
+        self.camera.release()
+        self.get_logger().info("Mipi Camera released.")
+        super().destroy_node()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = MipiCameraPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- src/sensor_interface/setup.py
+++ src/sensor_interface/setup.py
@@ -24,7 +24,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'mipi_camera = sensor_interface.mipi_camera:main',
+            'mipi_camera_publisher = sensor_interface.mipi_camera:main',
         ],
     },
 )
