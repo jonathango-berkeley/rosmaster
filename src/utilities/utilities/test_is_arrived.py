995253971662344de5d469ff1da93d02eaad4f8a
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-04-08 11:51:48 -0700

    add test script

---
+++ src/utilities/utilities/test_is_arrived.py
@@ -0,0 +1,13 @@
+from rescue_robot import RescueRobot
+
+robot = RescueRobot()
+
+initial_position = robot.get_position()
+
+input("move robot to a different location through rviz and hit enter")
+
+robot.run_robot(initial_position)
+
+if robot.is_arrived():
+    print("Robot is Arrived")
+
