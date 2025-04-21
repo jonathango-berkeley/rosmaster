7d555eb04b04f1014d509f8efa1ffc5808902c69
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-04-21 05:19:47 -0700

    move test to inside rescue_robot

--- src/utilities/utilities/rescue_robot.py
+++ src/utilities/utilities/rescue_robot.py
@@ -203,5 +203,21 @@ def main():
     signal.signal(signal.SIGINT, clean_exit)
     robot = RescueRobot()
 
+    while True:
+        if robot.current_position is not None:
+            print(f"current position: {robot.current_position.transform.translation}")
+        else:
+            print("no position found")
+
+        try:
+            print("----- aruco queue ------")
+            for key in robot.aruco_queue:
+                loc = robot.aruco_queue[key]["location"]
+                print(f"{key} location: {loc.transform.translation}")
+        except Exception as e:
+            print(f"error: {e}")
+
+        time.sleep(1)
+
 if __name__ == '__main__':
     main()
