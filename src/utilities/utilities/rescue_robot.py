711cbd326acab3518128708dae3333be289df179
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-04-21 06:34:04 -0700

    debug msgs

--- src/utilities/utilities/rescue_robot.py
+++ src/utilities/utilities/rescue_robot.py
@@ -211,6 +211,8 @@ def main():
     robot = RescueRobot()
 
     while True:
+        print('wait for aruco_detection')
+        print(robot.aruco_queue)
         if robot.aruco_queue:
             for key in robot.aruco_queue:
                 robot.run_robot(robot.aruco_queue[key]['location'])
