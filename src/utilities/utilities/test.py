cfa55020c97873aee20a4b516287574a7e91b9a5
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-04-21 05:08:18 -0700

    add test.py

--- src/utilities/utilities/test.py
+++ src/utilities/utilities/test.py
@@ -2,8 +2,17 @@ from rescue_robot import RescueRobot
 
 robot = RescueRobot()
 
-origin = robot.get_position()
+while True:
+    if robot.current_position is not None:
+        print(f"current position: {robot.current_position.transform.translation}")
+    else:
+        print("no position found")
 
-input('enter!')
+    try:
+        print("----- aruco queue ------")
+        for key in robot.aruco_queue:
+            loc = robot.aruco_queue[key]["location"]
+            print(f"{key} location: {loc.transform.translation}")
+    except Exception as e:
+        print(f"error: {e}")
 
-robot.run_robot(origin)
