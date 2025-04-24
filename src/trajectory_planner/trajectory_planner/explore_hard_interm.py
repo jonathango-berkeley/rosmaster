e0610acabb2b18b26594c62146da16ad59448d1b
Gustav-Friede <gustav99@berkeley.edu>
2025-04-23 17:33:51 -0700

    changes applied to exploration

--- src/trajectory_planner/trajectory_planner/explore_hard_interm.py
+++ src/trajectory_planner/trajectory_planner/explore_hard_interm.py
@@ -33,7 +33,7 @@ class ExploreHard(Node):
         self.publisher = self.create_publisher(PoseStamped, '/goal_pose', 10)
         
         self.prev_waypoint = None    #previous waypoint
-        self.curr_waypoint = None    #current waypoint
+        self.waypoint_coords = None    #coordinates of the waypoint we currently want to move to
         self.goal_waypoint = None    #goal waypoint
         self.count_interm_wayp = 0    #count of intermediate waypoint
         self.interm_wayp = []    #list of intermediate waypoints
@@ -43,79 +43,78 @@ class ExploreHard(Node):
 
 
     def publish_waypoint(self, sub_msg):
-        if sub_msg.data:    #goes in if msg is true
             
-            #set goal waypoint
-            if self.goal_waypoint is None:
-                self.prev_waypoint = "base"
-                self.goal_waypoint = "A"
-            elif waypoints[self.goal_waypoint] != self.curr_waypoint:
-                self.count_interm_wayp += 1
+        #set goal waypoint
+        if self.goal_waypoint is None:
+            self.prev_waypoint = 0
+            self.goal_waypoint = 1
+        elif waypoints[self.goal_waypoint] != self.curr_waypoint:
+            self.count_interm_wayp += 1
+        else:
+            if self.goal_waypoint == 0:
+                self.get_logger().info("Exploration finished!")
+                return None    #ends exploration (the script could also be restarted here)
             else:
-                if self.goal_waypoint == "base":
-                    next_key = "A"    #restarts exploration (the script could also be stopped here)
-                else:
-                    next_key = chr(ord(self.goal_waypoint) + 1)    #next unicode character
-                self.count_interm_wayp = 0
-                self.interm_wayp.clear()
-                if next_key in waypoints:    #test if key is in waypoints
-                    self.prev_waypoint = self.goal_waypoint
-                    self.goal_waypoint = next_key
-                else:
-                    self.prev_waypoint = self.goal_waypoint
-                    self.goal_waypoint = "base"
-                    self.get_logger().info("Return to the base!")
+                next_key = self.goal_waypoint + 1
+            self.count_interm_wayp = 0
+            self.interm_wayp.clear()
+            if next_key < len(waypoints):    #test if key is in waypoints
+                self.prev_waypoint = self.goal_waypoint
+                self.goal_waypoint = next_key
+            else:
+                self.prev_waypoint = self.goal_waypoint
+                self.goal_waypoint = 0
+                self.get_logger().info("Return to the base!")
             
-            #set waypoint (intermediate or goal)
-            if not self.interm_wayp:
+        #set waypoint (intermediate or goal)
+        if not self.interm_wayp:
                 
-                #transform goal_waypoint and prev_waypoint into map coordinates (cells)
-                res = self.current_map.info.resolution
-                goal_x = int(waypoints[self.goal_waypoint][0]/res)
-                goal_y = int(waypoints[self.goal_waypoint][1]/res)
-                prev_x = int(waypoints[self.prev_waypoint][0]/res)
-                prev_y = int(waypoints[self.prev_waypoint][1]/res)
+            #transform goal_waypoint and prev_waypoint into map coordinates (cells)
+            res = self.current_map.info.resolution
+            goal_x = int(waypoints[self.goal_waypoint][0]/res)
+            goal_y = int(waypoints[self.goal_waypoint][1]/res)
+            prev_x = int(waypoints[self.prev_waypoint][0]/res)
+            prev_y = int(waypoints[self.prev_waypoint][1]/res)
+            
+            #plan path
+            #a_star.plot(self.current_map, [prev_x, prev_y], [goal_x, goal_y])
+            trajectory = a_star.a_star(self.current_map, [prev_x, prev_y], [goal_x, goal_y])
                 
-                #plan path
-                #a_star.plot(self.current_map, [prev_x, prev_y], [goal_x, goal_y])
-                trajectory = a_star.a_star(self.current_map, [prev_x, prev_y], [goal_x, goal_y])
+            #extract intermediate waypoints
+            k, m = divmod(len(trajectory), num_interm_wayp + 1)
+            parts = [trajectory[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(num_interm_wayp + 1)]    #dividing the list of waypoints
+            waypoints_map = [parts[i][-1] for i in range(num_interm_wayp)]    #extracting intermediate waypoints
                 
-                #extract intermediate waypoints
-                k, m = divmod(len(trajectory), num_interm_wayp + 1)
-                parts = [trajectory[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(num_interm_wayp + 1)]    #dividing the list of waypoints
-                waypoints_map = [parts[i][-1] for i in range(num_interm_wayp)]    #extracting intermediate waypoints
+            #extracting cell coordinates and transform back to real world coordinates
+            for i in range(len(waypoints_map)):
+                self.interm_wayp.append([waypoints_map[i].y * res, waypoints_map[i].x * res])
                 
-                #extracting cell coordinates and transform back to real world coordinates
-                for i in range(len(waypoints_map)):
-                    self.interm_wayp.append([waypoints_map[i].y * res, waypoints_map[i].x * res])
-                
-                self.interm_wayp.append(waypoints[self.goal_waypoint])
-                self.curr_waypoint = self.interm_wayp[self.count_interm_wayp]
-            else:
-                self.curr_waypoint = self.interm_wayp[self.count_interm_wayp]
+            self.interm_wayp.append(waypoints[self.goal_waypoint])
+            self.curr_waypoint = self.interm_wayp[self.count_interm_wayp]
+        else:
+            self.curr_waypoint = self.interm_wayp[self.count_interm_wayp]
+        pub_msg = PoseStamped()
+        pub_msg.header.stamp = self.get_clock().now().to_msg()
+        pub_msg.header.frame_id = "map"
+         
+        pub_msg.pose.position.x = float(self.curr_waypoint[0])
+        pub_msg.pose.position.y = float(self.curr_waypoint[1])
+            
+        #calculate orientation (facing towards the center)
+        if self.curr_waypoint != waypoints[4]:
+            dx = waypoints[4][0] - self.curr_waypoint[0]
+            dy = waypoints[4][1] - self.curr_waypoint[1]
+        else:
+            dx = waypoints[0][0] - self.curr_waypoint[0]
+            dy = waypoints[0][1] - self.curr_waypoint[1]
+        theta = math.atan2(dy, dx)
 
-            pub_msg = PoseStamped()
-            pub_msg.header.stamp = self.get_clock().now().to_msg()
-            pub_msg.header.frame_id = "map"
+        pub_msg.pose.orientation.z = math.sin(theta / 2.0)  #orientation?
+        pub_msg.pose.orientation.w = math.cos(theta / 2.0)  #orientation?
             
-            pub_msg.pose.position.x = float(self.curr_waypoint[0])
-            pub_msg.pose.position.y = float(self.curr_waypoint[1])
-            
-            #calculate orientation (facing towards the center)
-            if self.curr_waypoint != waypoints["D"]:
-                dx = waypoints["D"][0] - self.curr_waypoint[0]
-                dy = waypoints["D"][1] - self.curr_waypoint[1]
-            else:
-                dx = waypoints["base"][0] - self.curr_waypoint[0]
-                dy = waypoints["base"][1] - self.curr_waypoint[1]
-            theta = math.atan2(dy, dx)
-
-            pub_msg.pose.orientation.z = math.sin(theta / 2.0)  #orientation?
-            pub_msg.pose.orientation.w = math.cos(theta / 2.0)  #orientation?
-            
-            #publish
-            self.publisher.publish(pub_msg)
-            self.get_logger().info(f'Published new waypoint: {self.curr_waypoint}')    
+        #publish
+        self.publisher.publish(pub_msg)
+        self.get_logger().info(f'Published new waypoint: {self.curr_waypoint}')    
         
     def map_callback(self, map_msg):
         self.get_logger().info("Received a map message.")
