297be20c048462356800106fd96bc9ee46c55616
Gustav-Friede <gustav99@berkeley.edu>
2025-04-27 18:03:10 -0700

    debugging

--- src/trajectory_planner/resource/a_star_Gutsav_v4.py
+++ src/trajectory_planner/resource/a_star_Gutsav_v4.py
@@ -6,7 +6,7 @@ from nav_msgs.msg import OccupancyGrid
 #########################################################
 
 #global parameter
-buffer = 5    #size of buffer zone
+buffer = 0    #size of buffer zone
 
 #########################################################
 
