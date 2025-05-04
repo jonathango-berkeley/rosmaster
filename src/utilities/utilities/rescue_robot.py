f118191d55fcd9f6a3aa447d489cfc49ce4493c0
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-05-04 13:02:01 -0700

    debug

--- src/utilities/utilities/rescue_robot.py
+++ src/utilities/utilities/rescue_robot.py
@@ -90,7 +90,7 @@ class RescueRobot:
         self.LineTolerance = 0.05
         self.RotationTolerance = math.radians(5)
 
-        self.Linear = 0.2
+        self.Linear = 0.05
         self.Angular = 0.1
 
         self.proportional_forward = 1 # Tweak P-controller
