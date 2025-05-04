aa7da97a6baaa2e06da6e371d24eb17e9de0beca
Jonathan Goenadibrata <jonathango@berkeley.edu>
2025-05-04 00:52:28 -0700

    new get_pose, explore and controller that kind of work

--- src/trajectory_planner/resource/explore_hard_interm.py
+++ src/trajectory_planner/resource/explore_hard_interm.py
@@ -29,7 +29,7 @@ class ExploreHard(Node):
             [0.0, -1.3],
             [0.7, -0.7]
         ]
-        self.num_interm_wayp = 2    #total number of intermediate waypoints (needs tuning)
+        #self.num_interm_wayp = 2    #total number of intermediate waypoints (needs tuning)
         self.prev_waypoint = 0    #previous waypoint list position
         self.waypoint_coords = None    #coordinates of the waypoint we currently want to move to
         self.goal_waypoint = 1    #goal waypoint list position
@@ -81,13 +81,13 @@ class ExploreHard(Node):
             trajectory = a_star.a_star(self.map_data, [prev_x, prev_y], [goal_x, goal_y])
                 
             #extract intermediate waypoints
-            k, m = divmod(len(trajectory), self.num_interm_wayp + 1)
-            parts = [trajectory[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.num_interm_wayp + 1)]    #dividing the list of waypoints
-            waypoints_map = [parts[i][-1] for i in range(self.num_interm_wayp)]    #extracting intermediate waypoints
+            #k, m = divmod(len(trajectory), self.num_interm_wayp + 1)
+            #parts = [trajectory[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(self.num_interm_wayp + 1)]    #dividing the list of waypoints
+            #waypoints_map = [parts[i][-1] for i in range(self.num_interm_wayp)]    #extracting intermediate waypoints
                 
             #extracting cell coordinates and transform back to real world coordinates
-            for i in range(len(waypoints_map)):
-                self.interm_wayp.append([(waypoints_map[i].y * res)+orix, (waypoints_map[i].x * res)+oriy])
+            for i in range(len(trajectory)):
+                self.interm_wayp.append([(trajectory[i].y * res)+orix, (trajectory[i].x * res)+oriy])
                 
             self.interm_wayp.append(self.waypoints[self.goal_waypoint])
 
